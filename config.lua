fee19ca2d27b32590b84a8bf98f0ff0bed1e527f
Charly <charly.strn@gmail.com>
2017-08-21 11:59:48 +0200

    initial commit

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Map = {
+  {name="Aéroport",color=15, id=90,x = -814.308,  y = -183.823,  z = 36.568},
+  {name="Tequil-La La",color=15, id=93, x=-565.171, y=276.625, z=83.286},
+  {name="Gouvernement",color=1, id=498, x=-429.46313476563, y=1109.98, z=327.682},
+  {name="Aéroport", color=15, id=90, x=1743.6820, y=3286.2510, z=40.087},
+  {name="Yacht",color=15, id=410, x=-2045.800, y=-1031.200, z=11.900},
+  {name="Cargoship",color=15, id=410, x=-90.000, y=-2365.800, z=14.300},
+  {name="Bahamas",color=15, id=93,x=-1388.3294677734, y =-585.99084472656, z= 30.216510772705}
+
+
+}
